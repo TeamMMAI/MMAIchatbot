4673e2f65826380b46134052d2e363720a4b7233
JaeHyunGround <gothddlek@naver.com>
2023-03-13 17:30:32 +0900

    fix mmai

--- src/components/ChatbotStart.js
+++ src/components/ChatbotStart.js
@@ -2,8 +2,15 @@ import React from 'react';
 import "../styles/components/ChatbotStart.css";
 
 const ChatbotStart = ({chectState, chatbotParam}) => {
+
+    const goChat = () => {
+        if(chectState) {
+            window.open("https://teammmai.github.io/MMAI_chatroom/chatbot_code.html")
+        }
+    }
+
     return (
-        <div className={(chectState) ? 'chat_start' : 'chat_not'}>
+        <div onClick={goChat} className={(chectState) ? 'chat_start' : 'chat_not'}>
             Selection Chatbot Complete
         </div>
     );
